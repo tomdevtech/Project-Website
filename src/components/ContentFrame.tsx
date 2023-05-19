b0a0fdb964f4fc9f4c085c7783a509d6b811a896
Tom@DESKTOP-3M3JSCQ <Tom.Helling1234@gmail.com>
2023-05-19 11:02:27 +0200

    Updated links to project.

--- src/components/ContentFrame.tsx
+++ src/components/ContentFrame.tsx
@@ -10,7 +10,7 @@ export default function ContentFrame({ title, descriptive_text, link }: Props) {
   return (
     <div className="ContentFrame">
         <div className="contentHeadline">{title}</div>
-        <div className="contentContent"><p>{descriptive_text}</p></div>
+        <div className="contentContent"><p>{descriptive_text}</p></div><br/><br/>
         <div className="contentLink"><a href={link}>Gehe zu Projekt {title}</a></div>
     </div>
   );
