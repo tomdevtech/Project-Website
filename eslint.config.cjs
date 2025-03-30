d1d8ff23fe0867fcad2474616ca1472525651b0d
Tom.DevTech <57768405+tomdevtech@users.noreply.github.com>
2025-03-30 22:22:46 +0200

    Update eslint.config.cjs

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,6 +1,5 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  root: true,
   env: {
     browser: true,
     es2021: true,
@@ -21,7 +20,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
   ],
   rules: {
-    'react/react-in-jsx-scope': 'off', // Weil du Vite benutzt, brauchst du kein `import React from 'react'`
+    'react/react-in-jsx-scope': 'off', // Für Vite, kein `import React from 'react'` notwendig
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     '@typescript-eslint/no-unused-vars': 'warn',
