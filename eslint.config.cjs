214d27c2d71b0b85d26839391a28859457e6a172
Tom.DevTech <57768405+tomdevtech@users.noreply.github.com>
2025-03-30 22:24:46 +0200

    Update eslint.config.cjs

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,34 +1,31 @@
+const js = require('@eslint/js');
+const globals = require('globals');
+const react = require('eslint-plugin-react');
+const reactHooks = require('eslint-plugin-react-hooks');
+const typescriptEslint = require('@typescript-eslint/eslint-plugin');
+const typescriptParser = require('@typescript-eslint/parser');
+
 /** @type {import('eslint').Linter.Config} */
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: globals.browser,
+      parser: typescriptParser,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['react', 'react-hooks', '@typescript-eslint'],
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  rules: {
-    'react/react-in-jsx-scope': 'off', // Für Vite, kein `import React from 'react'` notwendig
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    '@typescript-eslint/no-unused-vars': 'warn',
-  },
-  settings: {
-    react: {
-      version: 'detect',
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      '@typescript-eslint': typescriptEslint,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
     },
   },
-  ignorePatterns: ['dist/', 'node_modules/', 'public/'],
-};
+];
