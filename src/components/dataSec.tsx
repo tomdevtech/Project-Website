674c402dbe7a622d33bf946f53a829e57ac09b5a
Tom.DevTech <Tom.Helling1234@gmail.com>
2025-04-12 15:20:01 +0200

    Update Responsive Behaviour of the language selection.

--- src/components/dataSec.tsx
+++ src/components/dataSec.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export function DataSec() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="background">
       <div className="ContentSubPage">
