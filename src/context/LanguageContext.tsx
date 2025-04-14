73bc08825bb512830c9b416139df7e8943670d53
Tom.DevTech <Tom.Helling1234@gmail.com>
2025-04-14 11:15:22 +0200

    Updated Language behaviour.

--- src/context/LanguageContext.tsx
+++ src/context/LanguageContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, ReactNode, useEffect } from 'react';
+import React, { createContext, useContext, useState, ReactNode } from 'react';
 
 type Language = 'de' | 'en';
 
